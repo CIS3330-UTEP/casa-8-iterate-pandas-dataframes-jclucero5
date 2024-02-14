4ae559eeb1dfb24bc06298a82ee3c076b53c7fdc
jclucero5 <Jclucero5@miners.utep.edu>
2024-02-14 01:12:38 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+df = pd.read_csv('big-mac-full-index.csv')
+
+for i,r in df.iterrows():
+   print(r['iso_a3'])
+   print(r['currency_code'])
+   print(r['local_price'])
+
+print(df.apply(lambda row: row['adj_price'], axis= 1))
+print(df.apply(lambda row: row['USD_adjusted'], axis = 1))
